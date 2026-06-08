ea2524120eea498e863323e1eeb28fe2e40d5a25
Yogesh Kumawat <yogeshkumawat2027@gmail.com>
2026-06-08 20:11:51 +0530

    Add Header component with basic structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { getTransactions } from "../services/transactionService";
+
+ function Header(){
+   
+    return(
+        <>
+        <h1>Header</h1>
+        </>
+    )
+ }
+
+ export default Header;
