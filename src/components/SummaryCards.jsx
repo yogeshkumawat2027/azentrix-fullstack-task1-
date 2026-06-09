c1a0bdd039b1685c8b1dc2b4ff17b0c9b6707b85
Yogesh Kumawat <yogeshkumawat2027@gmail.com>
2026-06-09 20:49:51 +0530

    Create SummaryCards component

---
+++ src/components/SummaryCards.jsx
@@ -0,0 +1,12 @@
+import { getTransactions } from "../services/transactionService";
+
+ function SummaryCards(){
+   
+    return(
+        <>
+        <h1>SummaryCards</h1>
+        </>
+    )
+ }
+
+ export default SummaryCards;
