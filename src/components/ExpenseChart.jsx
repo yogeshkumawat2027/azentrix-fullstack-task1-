0927559ccd5ffaf8e962a3b69815eb20f397fa04
Yogesh Kumawat <yogeshkumawat2027@gmail.com>
2026-06-10 22:01:54 +0530

    Update ExpenseChart.jsx

---
+++ src/components/ExpenseChart.jsx
@@ -0,0 +1,12 @@
+import { getTransactions } from "../services/transactionService";
+
+ function ExpanseChart(){
+
+    return(
+        <>
+        <h1>Header</h1>
+        </>
+    )
+ }
+
+ export default ExpanseChart;
