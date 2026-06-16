6347d0e7c3316aed9435324c5a2bef2c59180c67
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-06-16 15:00:38 +0530

    added header  into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
-import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-// import './App.css'
-
-import {
-  getTransactions,
-} from "./services/transactionService";
-
-import TransactionForm from "./components/TransactionForm";
-// import TransactionTable from "./components/TransactionTable";
-// import SummaryCards from "./components/SummaryCards";
-// import ExpenseChart from "./components/ExpenseChart";
+import Header from "./components/Header";
+import { getTransactions } from "./services/transactionService";
 
 function App() {
-  
+  const transactions = getTransactions();
 
   return (
     <div>
-     
-
-      {/* <SummaryCards transactions={transactions} /> */}
-
-      <TransactionForm
-        transactions={transactions}
-        setTransactions={setTransactions}
-      />
-{/* 
-      <TransactionTable
-        transactions={transactions}
-        setTransactions={setTransactions}
-      />
-
-      <ExpenseChart transactions={transactions} /> */}
+      <Header transactions={transactions} />
     </div>
   );
 }
